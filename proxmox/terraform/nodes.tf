8229736f45ac5f3bc0add7dc93a17bb26877e314
rkschamer <26967205+rkschamer@users.noreply.github.com>
2025-12-02 18:23:49 +0000

    (tf) making first TF plan work

--- proxmox/terraform/nodes.tf
+++ proxmox/terraform/nodes.tf
@@ -1,153 +1,36 @@
-# Talos Control Plane Node
-resource "proxmox_vm_qemu" "talos_control_01" {
-  # VM General Settings
-  name        = "talos-control-01"
-  target_node = var.proxmox_node
-  vmid        = 101
-  os_type     = "other"
-  iso         = var.talos_iso_path
-
-  # VM System Settings
-  agent = 1
-
-  # VM Hardware Settings
-  cores   = 4
-  sockets = 1
-  memory  = 6144
-  scsihw  = "virtio-scsi-pci"
-  boot    = "order=scsi0;ide2" # Boot from disk first, then CD-ROM (for install)
-  disk {
-    type    = "scsi"
-    storage = "local-lvm"
-    size    = "40G"
-  }
-
-  # VM Network Settings
-  network {
-    model  = "virtio"
-    bridge = "vmbr0"
-  }
-
-  # Talos Machine Configuration (replaces Cloud-Init)
-  cloudinit {
-    user_data = file("${var.talos_config_path}/controlplane.yaml")
-  }
-}
-
-# Talos Trusted Worker Node
-resource "proxmox_vm_qemu" "talos_worker_trusted" {
-  # VM General Settings
-  name        = "talos-worker-trusted"
-  target_node = var.proxmox_node
-  vmid        = 201
-  os_type     = "other"
-  iso         = var.talos_iso_path
-
-  # VM System Settings
-  agent = 1
-
-  # VM Hardware Settings
-  cores   = 4
-  sockets = 1
-  memory  = 10240
-  scsihw  = "virtio-scsi-pci"
-  boot    = "order=scsi0;ide2"
-  disk {
-    type    = "scsi"
-    storage = "local-lvm"
-    size    = "50G"
-  }
-
-  # VM Network Settings (Dual NIC)
-  network {
-    model  = "virtio"
-    bridge = "vmbr1"
-  }
-  network {
-    model  = "virtio"
-    bridge = "vmbr0"
-  }
-
-  # Talos Machine Configuration
-  cloudinit {
-    user_data = file("${var.talos_config_path}/worker-trusted.yaml")
-  }
-}
-
-# Talos DMZ Worker Node
-resource "proxmox_vm_qemu" "talos_worker_dmz" {
-  # VM General Settings
-  name        = "talos-worker-dmz"
-  target_node = var.proxmox_node
-  vmid        = 202
-  os_type     = "other"
-  iso         = var.talos_iso_path
-
-  # VM System Settings
-  agent = 1
-
-  # VM Hardware Settings
-  cores   = 4
-  sockets = 1
-  memory  = 8192
-  scsihw  = "virtio-scsi-pci"
-  boot    = "order=scsi0;ide2"
-  disk {
-    type    = "scsi"
-    storage = "local-lvm"
-    size    = "40G"
-  }
-
-  # VM Network Settings (Dual NIC)
-  network {
-    model  = "virtio"
-    bridge = "vmbr2"
-  }
-  network {
-    model  = "virtio"
-    bridge = "vmbr0"
-  }
-
-  # Talos Machine Configuration
-  cloudinit {
-    user_data = file("${var.talos_config_path}/worker-dmz.yaml")
-  }
-}
-
-# Talos Untrusted Worker Node
-# Control Plane Node
+# Control Plane Nodes
 resource "proxmox_vm_qemu" "control_plane" {
-  for_each = { for cp in var.control_plane_nodes : cp.name => cp }
+  for_each = { for node in var.control_plane_nodes : node.name => node }
 
   name        = each.value.name
   target_node = var.proxmox_node
-
-  iso = var.talos_iso_path
+  clone       = var.talos_template_name
 
   # VM settings
   agent   = 1
   os_type = "cloud-init"
-  cores   = each.value.cores
-  sockets = 1
   memory  = each.value.memory
-  scsihw  = "virtio-scsi-pci"
+  cpu {
+    sockets = 1
+    cores   = each.value.cores
+  }
 
   disk {
-    type    = "scsi"
+    slot    = "scsi0"
     storage = "local-lvm"
     size    = each.value.disk_size
   }
 
   network {
-    model   = "virtio"
-    bridge  = each.value.bridge
-    macaddr = each.value.mac_address
+    id     = 0
+    model  = "virtio"
+    bridge = each.value.network_bridge
   }
 
+  ipconfig0 = "ip=${each.value.ip_address}/24,gw=${var.gateway}"
+
   # Cloud-init settings for Talos
-  cloudinit {
-    user_data = file("${var.talos_config_path}/controlplane.yaml")
-  }
+  cicustom = "user=${var.talos_config_path}/controlplane.yaml"
 }
 
 # Worker Nodes
@@ -156,31 +39,31 @@ resource "proxmox_vm_qemu" "worker" {
 
   name        = each.value.name
   target_node = var.proxmox_node
-
-  iso = var.talos_iso_path
+  clone       = var.talos_template_name
 
   # VM settings
   agent   = 1
   os_type = "cloud-init"
-  cores   = each.value.cores
-  sockets = 1
   memory  = each.value.memory
-  scsihw  = "virtio-scsi-pci"
 
+  cpu {
+    sockets = 1
+    cores   = each.value.cores
+  }
   disk {
-    type    = "scsi"
+    slot    = "scsi0"
     storage = "local-lvm"
     size    = each.value.disk_size
   }
 
   network {
-    model   = "virtio"
-    bridge  = each.value.bridge
-    macaddr = each.value.mac_address
+    id     = 0
+    model  = "virtio"
+    bridge = each.value.network_bridge
   }
 
+  ipconfig0 = "ip=${each.value.ip_address}/24,gw=${var.gateway}"
+
   # Cloud-init settings for Talos
-  cloudinit {
-    user_data = file("${var.talos_config_path}/worker.yaml")
-  }
+  cicustom = "user=${var.talos_config_path}/worker.yaml"
 }
--- proxmox/terraform/variables.tf
+++ proxmox/terraform/variables.tf
@@ -21,26 +21,31 @@ variable "proxmox_node" {
   default     = "proxmox" # Change this to your node's name
 }
 
+variable "talos_template_name" {
+  type        = string
+  description = "The name of the Talos VM template to clone."
+}
+
+variable "gateway" {
+  type        = string
+  description = "The gateway IP address for the VMs."
+}
+
 variable "talos_config_path" {
   type        = string
   description = "The path to the directory containing the Talos machine config files."
   default     = "../homelab-cluster/talos"
 }
 
-variable "talos_iso_path" {
-  type        = string
-  description = "The path in Proxmox storage to the Talos installer ISO (e.g., local:iso/proxmox-amd64.iso)."
-}
-
 variable "control_plane_nodes" {
   description = "A list of control plane node configurations."
   type = list(object({
-    name        = string
-    mac_address = string
-    disk_size   = string
-    memory      = number
-    cores       = number
-    bridge      = string
+    name           = string
+    ip_address     = string
+    disk_size      = string
+    memory         = number
+    cores          = number
+    network_bridge = string
   }))
   default = []
 }
@@ -48,12 +53,12 @@ variable "control_plane_nodes" {
 variable "worker_nodes" {
   description = "A list of worker node configurations."
   type = list(object({
-    name        = string
-    mac_address = string
-    disk_size   = string
-    memory      = number
-    cores       = number
-    bridge      = string
+    name       = string
+    ip_address = string
+    disk_size  = string
+    memory     = number
+    cores      = number
+    network_bridge = string
   }))
   default = []
 }
