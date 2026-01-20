4c8284825c6e28c1a6110da97161e84fe001b364
rkschamer <26967205+rkschamer@users.noreply.github.com>
2026-01-20 10:14:32 +0000

    (talos) complete cilium and flux bootstrap

--- proxmox/terraform/talos/bootstrap/bootstrap.sh
+++ proxmox/terraform/talos/bootstrap/bootstrap.sh
@@ -7,14 +7,25 @@ kubectl taint nodes --all node.cilium.io/agent-not-ready- --overwrite || true
 echo "Installing Cilium CLI..."
 echo "Installing Cilium with eBPF, Hubble and host firewall enabled..."
 cilium install \
-  --datapath-mode ebpf \
-  --enable-hubble \
-  --enable-host-firewall \
-  --enable-hubble-ui
+    --set ipam.mode=kubernetes \
+    --set kubeProxyReplacement=true \
+    --set securityContext.capabilities.ciliumAgent="{CHOWN,KILL,NET_ADMIN,NET_RAW,IPC_LOCK,SYS_ADMIN,SYS_RESOURCE,DAC_OVERRIDE,FOWNER,SETGID,SETUID}" \
+    --set securityContext.capabilities.cleanCiliumState="{NET_ADMIN,SYS_ADMIN,SYS_RESOURCE}" \
+    --set cgroup.autoMount.enabled=false \
+    --set cgroup.hostRoot=/sys/fs/cgroup \
+    --set k8sServiceHost=localhost \
+    --set k8sServicePort=7445 \
+    --set hubble.relay.enabled=true \
+    --set gatewayAPI.enabled=true \
+    --set gatewayAPI.enableAlpn=true \
+    --set gatewayAPI.enableAppProtocol=true
 
 echo "Cilium installation completed."
 echo "Waiting for Cilium to be fully ready..."
 cilium status --wait
 
+echo "Showing Cilium ConfigMap. Check if this needs to be exported for GitOps into flux/installs/cilium-config.yaml"
+kubectl get cm -n kube-system cilium-config -o yaml
+
 echo "Bootstrapping FluxCD to GitHub repository..."
-flux bootstrap github --owner=rkschamer --repository=homelab --branch=main --path=./flux --personal
+flux bootstrap github --owner=rkschamer --repository=homelab --branch=main --path=./flux --personal --token-auth=false
