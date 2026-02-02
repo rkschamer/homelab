4d8797c742357fe703ea2a18078a6fd37f37b018
rkschamer <26967205+rkschamer@users.noreply.github.com>
2026-02-02 17:46:15 +0000

    (k8s) add hubble replay network policies

--- talos/bootstrap/bootstrap.sh
+++ talos/bootstrap/bootstrap.sh
@@ -7,6 +7,7 @@ kubectl taint nodes --all node.cilium.io/agent-not-ready- --overwrite || true
 echo "Installing Cilium CLI..."
 echo "Installing Cilium with eBPF, Hubble and host firewall enabled..."
 cilium install \
+    --set cluster.name=homelab \
     --set ipam.mode=kubernetes \
     --set kubeProxyReplacement=true \
     --set securityContext.capabilities.ciliumAgent="{CHOWN,KILL,NET_ADMIN,NET_RAW,IPC_LOCK,SYS_ADMIN,SYS_RESOURCE,DAC_OVERRIDE,FOWNER,SETGID,SETUID}" \
@@ -16,9 +17,7 @@ cilium install \
     --set k8sServiceHost=localhost \
     --set k8sServicePort=7445 \
     --set hubble.relay.enabled=true \
-    --set gatewayAPI.enabled=true \
-    --set gatewayAPI.enableAlpn=true \
-    --set gatewayAPI.enableAppProtocol=true \
+    --set hubble.ui.enabled=true \
     --set hostFirewall.enabled=true
 
 echo "Cilium installation completed."
